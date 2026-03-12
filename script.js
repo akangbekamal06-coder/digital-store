39148a33a50ffb6508a207f106047f9ac4a2bae6
Digital Hub <akangbekamal06@gmail.com>
2026-03-12 14:42:40 +0000

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function buyProduct(product){
-localStorage.setItem("product",product)
-window.location="payment.html"
-}
-
-function completePayment(){
-let product=localStorage.getItem("product")
-document.getElementById("paymentMessage").innerText="Payment successful. Your "+product+" will be delivered."
-}
